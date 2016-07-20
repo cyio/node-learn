106159bde3cc27bbdd338c8e6ceea629cb68e809
cyio <icaner@qq.com>
2016-07-20 15:30:12 +0800

    完成Buffer拼接

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const fs = require('fs')
+const iconv = require('iconv-lite')
 
 const rs = fs.createReadStream('test.md', {highWaterMark: 11})
 let data = ''
+let chunks = []
+let size = 0
 rs.on('data', (chunk)=>{
-	data += chunk
+	chunks.push(chunk)
+	size += chunk.length
 })
 rs.on('end', ()=>{
-	console.log(data)
+	const buf = Buffer.concat(chunks, size)
+	const str = iconv.decode(buf, 'utf8')
+	console.log(str)
 })
